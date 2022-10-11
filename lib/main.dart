0f045ef13cf3bbfb949954519d167de7199a60d4
Silvia Amanda <72431542+Silbivi@users.noreply.github.com>
2022-10-11 11:50:58 +0700

    lib

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:login_flutter/constants.dart';
+import 'package:login_flutter/screen/splash_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        fontFamily: "Muli",
+        textTheme: TextTheme(
+          bodyText1: TextStyle(color: kTextColor),
+          bodyText2: TextStyle(color: kTextColor),
+        ),
+        visualDensity: VisualDensity.adaptivePlatformDensity
+      ),
+      home: SplashScreen(),
+    );
+  }
+}
+
---
+++ lib/screen/splash_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:login_flutter/screen/component/body.dart';
+import 'package:login_flutter/size_config.dart';
+
+class SplashScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: Body(),
+    );
+  }
+}
